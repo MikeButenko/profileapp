1130597bae16fcba94995ba5c87dcdefde0cdc89
Ukrainianboy <mihabutenko1996@gmail.com>
2017-07-06 09:06:42 +0300

    Update context_processors.py

--- profileapp/context_processors.py
+++ profileapp/context_processors.py
@@ -5,9 +5,4 @@ def context_proc(request):
     return {
         'settings': settings,
         'ip_address': request.META['REMOTE_ADDR'],
-        'mime': request.META['CONTENT_TYPE']
     }
-
-
-def get_models_object(request):
-    pass
